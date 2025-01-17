c08ec3db09031b34f04c317f15d3e3e53063dcc2
JohnColleen <johns@n-compass.biz>
2025-01-17 16:54:05 +0800

    websocket inital commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const {createServer} = require("http");
+const {Server} = require("socket.io");
+
+const httpServer = createServer();
+const socket = new Server(httpServer, {
+	/* options */
+	cors: {
+		origin: "http://127.0.0.1:5500",
+	},
+});
+
+socket.on("connection", () => {
+	// ...
+	console.log(socket);
+});
+
+httpServer.listen(3000, () => {
+	console.log("Server is connected");
+});
